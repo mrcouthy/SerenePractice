92779241f27db2682906dd75a4befb04c6d49918
adf <asdf@adsf.com>
2016-08-02 17:13:20 +0545

    Added migration for a new table

---
+++ MovieTutorial/MovieTutorial.Web/Modules/Common/Migrations/DefaultDB/DefaultDB_20160802_115400_Employee.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+
+namespace MovieTutorial.Migrations.DefaultDB
+{
+    [Migration(20160802115400)]
+    public class DefaultDB_20160802_115400_Employee : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Employee").InSchema("mov")
+                .WithColumn("EmployeeId").AsInt32().Identity().PrimaryKey().NotNullable()
+                .WithColumn("Name").AsString().NotNullable()
+                .WithColumn("Address").AsInt32().NotNullable()
+                .WithColumn("Email").AsString().Nullable()
+                ;
+
+          
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
